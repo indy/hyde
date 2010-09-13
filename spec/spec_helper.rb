b60bd92f829241610b22a3dcc9bfce4ad4c2ec86
Inderjit Gill <email@indy.io>
2010-09-13 11:33:55 +0100

    simplified spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,6 @@ require 'yaml'
 
 require 'hyde'
 
-require 'hyde/core_ext'
-require 'hyde/filters'
-require 'hyde/convertible'
-require 'hyde/page'
 # require 'spec'
 # require 'spec/interop/test'
 
